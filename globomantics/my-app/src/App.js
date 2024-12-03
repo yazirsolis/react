a25ac58a1a2498ad4fcc0c8b306aadd484bd38e2
Yazir Solis <yazir.solis@gmail.com>
2024-12-03 10:37:40 -0800

    saving updates

---
+++ globomantics/my-app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Button from './components/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Sample react demo
+        </p>
+        <Button />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ globomantics/my-app/src/components/Button.js
@@ -0,0 +1,14 @@
+function Button() {
+    return (
+        <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            Click here
+        </a>
+    );
+}
+
+export default Button;
